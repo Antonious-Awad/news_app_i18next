1695169b9a91910f1ac056ea67ac4fbdac9d8ef2
Antonious-Awad <antonuostony1@gmail.com>
2024-06-20 15:01:23 +0300

    feat:: init app with layout

---
+++ app/components/LanguageSwitcher.tsx
@@ -0,0 +1,10 @@
+export default function LanguageSwitcher() {
+  return (
+    <div className="flex space-x-2">
+      <select className="p-2 border border-red-900 focus:ring-2 focus:outline-none focus:ring-red-700 rounded-md cursor-pointer">
+        <option value="en">English</option>
+        <option value="es">Español</option>
+      </select>
+    </div>
+  );
+}
